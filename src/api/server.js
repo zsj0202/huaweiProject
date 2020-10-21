59cb1e1ff92a6d7c8b763bfeb1a99e6ff44f9963
zsj <332308436@qq.com>
2020-10-21 10:57:01 +0800

    完成详情页的开发

--- src/api/server.js
+++ src/api/server.js
@@ -14,12 +14,26 @@ define(['jquery'], function($) {
     function getPadData(){
         return $.ajax('/api/mock/pad.json');
     }
+    function getDetailData(type , id){
+        var promise = new Promise(function(resolve , reject){
+            $.ajax(`/api/mock/${type}.json`).then((res)=>{
+                // console.log(res);
+                for(var i = 0; i<res.goods_list.length; i++){
+                    if(id == res.goods_list[i].goodsId){
+                        resolve(res.goods_list[i]);
+                    }
+                }
+            });
+        });
+        return promise;
+    }
     
     return {
         getBannerData,
         getBanner2Data,
         getPhoneData,
         getBookData,
-        getPadData
+        getPadData,
+        getDetailData
     };
 });
